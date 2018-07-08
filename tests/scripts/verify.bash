a7fbbe933671411b712c288802483dff41ddb604
Egor Duplensky <falypr1nc3@gmail.com>
2018-07-08 18:14:36 +0300

    Test Travic-ci functionality.
    A lot of new packages added.

--- tests/scripts/verify.bash
+++ tests/scripts/verify.bash
@@ -1,2 +1,7 @@
-##!/usr/bin/env bash
-emacs --batch -f package-initialize -l tests/scripts/use-package-lint-verify.el my-init.org
+#!/usr/bin/env bash
+git_root=$(git rev-parse --show-toplevel)
+
+ln -s "$git_root"/tests/ ~/.emacs.d
+ls -lah ~/
+emacs --version
+emacs --batch -f package-initialize -l "$git_root"/tests/scripts/use-package-lint-verify.el "$git_root"/my-init.org
