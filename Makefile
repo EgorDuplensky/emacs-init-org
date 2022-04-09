2d4153a6efa58194b2f88fab2695aba7555fda57
Egor Duplensky <egor.duplensky@gmail.com>
2022-04-10 00:05:00 +0300

    Multiple configuration updates

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	emacs -Q --batch --eval "(setq debug-on-error t)" -l ./init.el
--- init.el
+++ init.el
@@ -4,12 +4,19 @@
 ;;; Code:
 ;;; Set up package
 (require 'package)
-;;(setq package-enable-at-startup nil)
+
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
         ("gnu" . "https://elpa.gnu.org/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 (setq package-native-compile t)
+
+;; always work with real files, not sym links
+(setq vc-follow-symlinks t)
+
+;; for example host specific proxy settings
+(if (file-exists-p "~/.emacs.d/host-specific-init.org")
+    (org-babel-load-file (expand-file-name "~/.emacs.d/host-specific-init.org")))
 
 (package-initialize)
 
@@ -21,14 +28,14 @@
   (package-install 'use-package))
 
 (require 'use-package)
+(setq use-package-verbose t)
 (setq use-package-always-ensure t)
-;; always work with real files, not sym links
-(setq vc-follow-symlinks t)
 ;; common configuratiom
 (org-babel-load-file (expand-file-name "~/.emacs.d/my-init.org"))
-;; work specific configuration
-(if (file-exists-p "~/.emacs.d/work-specific-settings.org")
-    (org-babel-load-file (expand-file-name "~/.emacs.d/work-specific-settings.org")))
 ;; autogenereated configuration (e.g. by use-package)
 (if (file-exists-p custom-file)
     (load custom-file))
+(put 'narrow-to-region 'disabled nil)
+
+(provide 'init)
+;;; init.el ends here
